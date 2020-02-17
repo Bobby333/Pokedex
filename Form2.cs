d8f9c7de24dfb7759d83dabc0b2698f20a101d95
Bobby333 <46753106+Bobby333@users.noreply.github.com>
2020-02-17 14:12:02 -0500

    Create Form2.cs

---
+++ Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pokemon
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public string Stat { get; set; }
+        public int Num{ get; set; }
+
+        private void Enter_Click(object sender, EventArgs e)
+        {
+           
+            Num =Convert.ToUInt16(value.Value);
+            Close();
+        }
+
+        private void stats_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Stat = stats.SelectedItem.ToString();
+
+        }
+    }
+}
