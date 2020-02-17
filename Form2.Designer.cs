33b027a3559a5336a61e2024aedd86cb0424acc7
Bobby333 <46753106+Bobby333@users.noreply.github.com>
2020-02-17 14:12:09 -0500

    Create Form2.Designer.cs

---
+++ Form2.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace pokemon
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stats = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.value = new System.Windows.Forms.NumericUpDown();
+            this.Enter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.value)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // stats
+            // 
+            this.stats.FormattingEnabled = true;
+            this.stats.Items.AddRange(new object[] {
+            "Atk",
+            "HP",
+            "Def",
+            "Spd",
+            "SpDef",
+            "SpAtk",
+            "Total"});
+            this.stats.Location = new System.Drawing.Point(187, 110);
+            this.stats.Name = "stats";
+            this.stats.Size = new System.Drawing.Size(358, 39);
+            this.stats.TabIndex = 0;
+            this.stats.SelectedIndexChanged += new System.EventHandler(this.stats_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(296, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose stat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(248, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(259, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Minimum stat value";
+            // 
+            // value
+            // 
+            this.value.Location = new System.Drawing.Point(243, 260);
+            this.value.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.value.Name = "value";
+            this.value.Size = new System.Drawing.Size(264, 38);
+            this.value.TabIndex = 3;
+            // 
+            // Enter
+            // 
+            this.Enter.Location = new System.Drawing.Point(265, 360);
+            this.Enter.Name = "Enter";
+            this.Enter.Size = new System.Drawing.Size(181, 78);
+            this.Enter.TabIndex = 4;
+            this.Enter.Text = "Enter";
+            this.Enter.UseVisualStyleBackColor = true;
+            this.Enter.Click += new System.EventHandler(this.Enter_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Enter);
+            this.Controls.Add(this.value);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stats);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.value)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox stats;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown value;
+        private System.Windows.Forms.Button Enter;
+    }
+}
